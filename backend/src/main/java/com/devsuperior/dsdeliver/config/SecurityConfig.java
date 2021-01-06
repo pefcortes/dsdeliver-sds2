2b1ce9c29451ae2af95ce9b859cfcc142824bcca
pefcortes <paulo-ernesto.cortes@hotmail.com>
2021-01-06 18:06:49 -0300

    Domain model

--- backend/src/main/java/com/devsuperior/dsdeliver/config/SecurityConfig.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/config/SecurityConfig.java
@@ -30,7 +30,6 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
 
-import org.hibernate.cfg.Environment;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
