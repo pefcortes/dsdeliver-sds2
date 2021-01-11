60ea534c90fa10db97c08d5d94ecd191fdaa55c1
pefcortes <paulo-ernesto.cortes@hotmail.com>
2021-01-11 17:05:40 -0300

    add Orders list

--- front-mobile/App.tsx
+++ front-mobile/App.tsx
@@ -1,10 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Header from './src/Header/Index';
 import { useFonts, OpenSans_400Regular, OpenSans_700Bold } from '@expo-google-fonts/open-sans';
 import AppLoading from 'expo-app-loading';
-import Home from './src/Home/Index';
+import Routes from './src/Routes'
+;
 
 export default function App() {
   let [fontsLoaded] = useFonts({
@@ -17,11 +17,12 @@ export default function App() {
  };
   
   return (
+    <>
     <View style={styles.container}>
       <StatusBar style="auto" />
-      <Header />
-      <Home />
+      < Routes />
     </View>
+    </>
   );
 }
 
--- front-mobile/src/Home/Index.tsx
+++ front-mobile/src/Home/Index.tsx
@@ -1,21 +1,25 @@
 import React from 'react';
+import { useNavigation } from '@react-navigation/native';
 import { StyleSheet, Text, View, Image } from 'react-native';
 import { RectButton } from 'react-native-gesture-handler';
+import Header from '../Header';
 
 function Home() {
-    const handleOnPress = () => {
-
-    }
-    
+  
+  const navigation = useNavigation();
+  const handleOnPress = () => {
+       navigation.navigate('Orders');
+  }
     return (
       <>
+    <Header />
     <View style={styles.container}>
     <Image source={require('../assets/deliveryman.png')} />
     <Text style={styles.title}> Acompanhe os pedidos e {'\n'} entregue no prazo. </Text>
     <Text style={styles.subTitle}>Receba todos os pedidos do seu {'\n'} restaurante na palma de sua mão</Text>
     </View>
     <View style={styles.footer}>
-        <RectButton style ={styles.button} onPress={}>
+        <RectButton style ={styles.button} onPress={handleOnPress}>
             <Text style={styles.buttonText}>VER PEDIDOS</Text>
         </RectButton>
     </View>
